3c53fdacb10af22e5601b4628f950c44f713b8ca
Михаил <messiahgamedesign@gmail.com>
2020-05-24 18:30:26 +0300

    Добавьте файлы проекта.

---
+++ medium8-1-6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace medium8_1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
